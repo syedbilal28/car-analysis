ef9682e19763ec2fb7bc239fce711ca2a18d285d
syedbilal28 <syed.bilal.sba@gmail.com>
2020-10-11 01:25:54 +0500

    All changes except views

--- car/analytics/test_pd.py
+++ car/analytics/test_pd.py
@@ -5,18 +5,26 @@ import os
 import pathlib
 from . import creator
 def set_Views():
+    # Create Connection with Databse
     engine=create_engine("sqlite:///"+"C:/Users/Bilal/PycharmProjects/car-analysis/car/db.sqlite3")
-    
+    #Get Current Working Directory
     cwd=str(os.getcwd())
+    #Read the received file
     df=pd.read_csv(cwd+'/file1.csv',error_bad_lines=False)
     df= df[["Make","Model","Year","Views","Days Online"]]
     clean_df=df.dropna()
+    clean_df["Year"] = df["Year"].astype(str)
+    #Calling the current database
     df= pd.read_sql_query(sql="SELECT * from Car",con=engine)
     df=df.drop('index',axis=1)
-    l=len(df.keys())-4
+    l=int((len(df.keys())-5)/2)
+    #Convert units into days
     for index,row in clean_df.iterrows():
         
         row["Days Online"]=row["Days Online"].strip()
+        row["Make"] = row["Make"].strip()
+        row["Model"] = row["Model"].strip()
+        row["Year"] = row["Year"].strip()
         if row["Days Online"][-4:]=="hour" or row["Days Online"][-5:]=="hours":
             num=eval(re.sub('\D','', row["Days Online"]))/24    
             clean_df.at[index,'Days Online']=num
@@ -31,32 +39,39 @@ def set_Views():
         clean_df.at[index,'Views']=eval(new)
     clean_df["Views"]=clean_df["Views"].astype(int)
     clean_df["Days Online"]=clean_df["Days Online"].astype(float)
-    aggregate_functions={"Views":'sum','Days Online':'mean'}
+    #Impose Count function
+    clean_df.loc[:, "Count"] = 1
+    #Grouping the dataframe
+    aggregate_functions={"Views":'mean','Days Online':'mean',"Count":"sum"}
     clean_df=clean_df.groupby(["Model","Make","Year"],as_index=False,sort=False).aggregate(aggregate_functions)
     clean_df["Year"]=clean_df["Year"].astype(object)
     to_process=clean_df
+    #Calculate Views Per Day
     clean_df['Views Per Day']=clean_df["Views"]/clean_df['Days Online']
     clean_df["Views Per Day"]=clean_df["Views Per Day"].astype(float)
     clean_df=clean_df.sort_values(by=["Views"],ascending=False)
     clean_df=clean_df.rename(columns={'Views Per Day':'Views Per Day {}'.format(l)})
+    clean_df = clean_df.rename(columns={'Views': 'Views {}'.format(l)})
     # df= pd.read_sql_query(sql="SELECT * from Car",con=engine)
     # df=df.drop('index',axis=1)
     final_df=pd.merge(clean_df,df,how="outer",on=["Model","Make",'Year'])
-    to_cal=get_fill_value(final_df)
-    final_df=final_df.fillna(value=to_cal)
+
+    final_df=final_df.fillna(value=0)
+    final_df["Days Online"] = (final_df["Days Online_x"] + final_df["Days Online_y"])
+    final_df["Count"] = (final_df["Count_x"] + final_df["Count_y"])
+    final_df = final_df.drop(['Days Online_x', 'Days Online_y','Count_x', 'Count_y'], axis=1)
     cols= list(final_df.filter(like='Views'))
     aggregate_functions={}
     for i in cols:
         if i[0:5]=="Views":
-            aggregate_functions[i]='sum'
+            aggregate_functions[i]='mean'
         elif i[0:4]=="Days":
-            aggregate_functions[i]='sum'
+            aggregate_functions[i]='mean'
             
 
     final_df=final_df.groupby(["Model","Make","Year"],as_index=False,sort=False).aggregate(aggregate_functions)
-    final_df["Views"]=((final_df["Views_x"]+final_df["Views_y"])*l).div(l+1)
     #final_df["Days Online"]=((final_df["Days Online_x"]+final_df["Days Online_y"])*l).div(l+1)
-    final_df=final_df.drop(['Views_x','Views_y'],axis=1)
+
     final_df=final_df.sort_values(by="Views",ascending=False)
     engine.execute("DROP TABLE Car")
     final_df.to_sql(con=engine,name="Cars")
@@ -86,6 +101,7 @@ def Set_Days_Online(clean_df):
     final_df.to_sql(con=engine,name="Car_Days")
 
 def get_fill_value(df):
+    '''Depreciated'''
     to_cal={}
     for i in df.columns:
         if i[0:5]=="Views":
